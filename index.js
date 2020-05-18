4ccfd77937e01546a972d21ae7c90de51508aaee
Ayush Arya <arya.ayush409@gmail.com>
2020-05-17 23:33:12 -0400

    debounce is added from lodash to stop chokidar to start

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 #!/usr/bin/env node
+
+const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
+
+const start = debounce(() => {
+	console.log('Starting User program');
+}, 100);
+chokidar
+	.watch('.')
+	.on('add', start)
+	.on('change', () => {
+		console.log('File changed');
+	})
+	.on('unlink', () => {
+		console.log('file unlinked');
+	});
