b0fdbc86fc8cd5f92a8ffeedd4dbb6da0138f392
Ayush Arya <arya.ayush409@gmail.com>
2020-05-18 00:12:09 -0400

    ensuring file exist

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
+const program = require('caporal');
+const fs = require('fs');
 
-const start = debounce(() => {
-	console.log('Starting User program');
-}, 100);
-chokidar
-	.watch('.')
-	.on('add', start)
-	.on('change', () => {
-		console.log('File changed');
-	})
-	.on('unlink', () => {
-		console.log('file unlinked');
-	});
+program.version('0.0.1').argument('[filename]', 'Name of a file to be execute').action(async ({ filename }) => {
+	const name = filename || 'index.js';
+	try {
+		await fs.promises.access(name);
+	} catch (err) {
+		throw new Error(`Could not find the file ${name}`);
+	}
+	const start = debounce(() => {
+		console.log('Starting User program');
+	}, 100);
+	chokidar.watch('.').on('add', start).on('change', start).on('unlink', start);
+});
+
+program.parse(process.argv);
